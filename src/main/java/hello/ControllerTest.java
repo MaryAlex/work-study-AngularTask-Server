d6fdf6434afbf31a99b73dab9c638782134d521b
MaryAlex <worker96@mail.ru>
2016-04-27 16:59:09 +0300

    tests

--- src/main/java/hello/ControllerTest.java
+++ src/main/java/hello/ControllerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,8 +31,16 @@ public class ControllerTest {
 
     @Test
     public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+        Obj obj = new Obj();
+        for (int i = 1; i < 21; i++) {
+            if (i != 20) {
+            }
+            obj.items.add(new Item(i));
+        }
+        obj.more = true;
+        ObjectMapper om = new ObjectMapper();
+        mvc.perform(MockMvcRequestBuilders.get("/bookmark").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+                .andExpect(content().string(equalTo(om.writeValueAsString(obj))));
     }
 }
